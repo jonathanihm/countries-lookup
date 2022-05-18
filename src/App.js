71a8621de69b8b79a91ca142f218167aa8350c5b
Jonathan <jonathan.ihm@gmail.com>
2022-05-17 19:12:49 -0500

    basic functionality of list all api and displaying on main page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      await fetch('http://localhost:3001/list')
+        .then((response) => response.json())
+        .then((data) => {
+          setCountries(data);
+          setLoading(false);
+        });
+    }
+
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {loading ? 'Loading' : countries.map((country) => (
+            <div key={country.name}>
+              {country.name}
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,19 @@
-const express = require('express')
+import fetch from 'node-fetch';
+import express from 'express';
+import cors from 'cors';
+
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('It works');
+let corsOptions = {
+  origin: 'http://localhost:3000'
+}
+
+app.get('/list', cors(corsOptions), async (req, res) => {
+    let data = {};
+    const response = await fetch('https://restcountries.com/v2/all');
+
+  res.send(await response.json());
 });
 
 app.listen(port, () => {
